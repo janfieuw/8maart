43c2e5219facb9cdc4e9426a2e0851333c813d71
Jan Fieuw <jan@docc.be>
2026-03-13 23:04:04 +0100

    my

--- src/components/app-shell.js
+++ src/components/app-shell.js
@@ -88,7 +88,7 @@ export default function AppShell({ session, children }) {
           <Box sx={{ display: "flex", alignItems: "center" }}>
             <Link href="/app/dashboard" style={{ display: "inline-flex" }}>
               <Image
-                src="/templates/logomypunctoooooo.png"
+                src="/templates/logomypunctooooooo.png"
                 alt="MyPunctoo"
                 width={180}
                 height={52}
